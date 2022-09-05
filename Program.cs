104c30ad39d8cef04f99a5568086b6c8298d771b
henderson-sga <henderson.pereira@sga.com.br>
2022-09-05 18:54:05 -0300

    init to test

---
+++ Program.cs
@@ -0,0 +1,72 @@
+using Microsoft.Azure.WebPubSub.AspNetCore;
+using Microsoft.Azure.WebPubSub.Common;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddWebPubSub(
+    o => o.ServiceEndpoint = new ServiceEndpoint(builder.Configuration["Azure:WebPubSub:ConnectionString"]))
+    .AddWebPubSubServiceClient<Sample_ChatApp>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{    
+    endpoints.MapGet("/negotiate", async (WebPubSubServiceClient<Sample_ChatApp> serviceClient, HttpContext context) =>
+    {
+        var id = context.Request.Query["id"];
+        if (id.Count != 1)
+        {
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsync("missing user id");
+            return;
+        }
+        var webpsInstance = serviceClient.GetClientAccessUri(userId: id); 
+        await context.Response.WriteAsync(webps.GetClientAccessUri());
+    });
+
+    endpoints.MapWebPubSubHub<Sample_ChatApp>("/eventhandler/{*path}");
+});
+
+app.Run();
+
+sealed class Sample_ChatApp : WebPubSubHub
+{
+    private readonly WebPubSubServiceClient<Sample_ChatApp> _serviceClient;
+
+    public Sample_ChatApp(WebPubSubServiceClient<Sample_ChatApp> serviceClient)
+    {
+        _serviceClient = serviceClient;
+    }
+
+    public override async Task OnConnectedAsync(ConnectedEventRequest request)
+    {
+        await _serviceClient.SendToAllAsync($"[SYSTEM] {request.ConnectionContext.UserId} joined.");
+    }
+
+    public override async ValueTask<UserEventResponse> OnMessageReceivedAsync(UserEventRequest request, CancellationToken cancellationToken)
+    {
+        await _serviceClient.SendToAllAsync($"[{request.ConnectionContext.UserId}] {request.Data}");
+
+        return request.CreateResponse($"[SYSTEM] ack.");
+    }
+
+    public override ValueTask<ConnectEventResponse> OnConnectAsync(ConnectEventRequest request, CancellationToken cancellationToken)
+    {
+      Console.WriteLine("SOMEONE TRYING TO CONNECT");
+      return base.OnConnectAsync(request, cancellationToken);
+    }
+
+    public override Task OnDisconnectedAsync(DisconnectedEventRequest request)
+    {
+      Console.WriteLine("SOMEONE DISCONNECTED");
+      return base.OnDisconnectedAsync(request);
+    }
+}
